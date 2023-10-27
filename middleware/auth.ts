5fe21223fdbfb44e0fee7f07f4a093f6f9e30289
David Sawatzky <david@diatonic.io>
2023-10-27 17:44:18 -0500

    checking auth

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,13 +1,14 @@
 /* eslint-disable */
 export default defineNuxtRouteMiddleware(async (to, from) => {
-  if (to.path !== '/') {
+  if (to.path !== '/' && to.path !== '/login') {
     const { onResult, onError } = useQuery(gql`
       query TokenCheck {
         tokenCheck
       }
     `)
     onResult(async (result) => {
-      if (!!result.data.tokenCheck === true) {
+      console.log(result)
+      if (result.data.tokenCheck === true) {
         await navigateTo(to.path)
       } else if (result.data.tokenCheck === 'unauthorized') {
         await navigateTo('/login')
