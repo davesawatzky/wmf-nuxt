b7621cf690c5bb3b601967d8f626a9b1d51d225c
David Sawatzky <david@diatonic.io>
2023-10-27 12:07:22 -0500

    changed apollo config file

--- apollo.config.js
+++ apollo.config.js
@@ -3,8 +3,8 @@ module.exports = {
     service: {
       name: 'wmf-nest',
       // URL to the GraphQL API
-      // url: 'https://wmf-diatonic-be.netlify.app/graphql',
-      url: 'http://localhost:3000/graphql',
+      url: 'https://wmf-diatonic-be.netlify.app/graphql',
+      // url: 'http://localhost:3000/graphql',
     },
     // Files processed by the extension
     includes: ['**/*.vue', 'graphql/**/*.gql', 'graphql/**/*.graphql'],
