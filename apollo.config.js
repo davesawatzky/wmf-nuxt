7ff038868beaedf11bedc6334a986312a79d4b76
David Sawatzky <david@diatonic.io>
2023-10-28 00:34:09 -0500

    change everything to diatonic dns

--- apollo.config.js
+++ apollo.config.js
@@ -3,7 +3,7 @@ module.exports = {
     service: {
       name: 'wmf-nest',
       // URL to the GraphQL API
-      url: 'https://wmf-diatonic-be.onrender.com/graphql',
+      url: 'https://wmf2024api.diatonic.ca/graphql',
       // url: 'http://localhost:3000/graphql',
     },
     // Files processed by the extension
