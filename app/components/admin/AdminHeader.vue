ae8efedaa16cae43f91b6862b6d7333e706035e0
David Sawatzky <david@diatonic.io>
2025-04-25 00:21:12 -0500

    removed avatar from header

--- app/components/admin/AdminHeader.vue
+++ app/components/admin/AdminHeader.vue
@@ -21,37 +21,6 @@
     :model="items"
     :pt="{ end: 'self-center' }">
     <template #start />
-    <template #item="{ item, props, hasSubmenu, root }">
-      <a
-        v-ripple
-        class="flex items-center"
-        v-bind="props.action">
-        <span><Icon :name="item.icon!" /></span>
-        <span class="ml-2">{{ item.label }}</span>
-        <PVBadge
-          v-if="item.badge"
-          :class="{ 'ml-auto': !root, 'ml-2': root }"
-          :value="item.badge" />
-        <span
-          v-if="item.shortcut"
-          class="ml-auto border border-surface-200 dark:border-surface-500 rounded-md bg-surface-100 dark:bg-surface-800 text-xs p-1"
-          >{{ item.shortcut }}</span
-        >
-        <i
-          v-if="hasSubmenu"
-          class="pi pi-angle-down text-primary-500 dark:text-primary-400-500 dark:text-primary-400"
-          :class="[
-            { 'pi-angle-down ml-2': root, 'pi-angle-right ml-auto': !root },
-          ]" />
-      </a>
-    </template>
-    <template #end>
-      <div class="flex items-center gap-2">
-        <PVAvatar
-          image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png"
-          shape="circle" />
-      </div>
-    </template>
   </PVMenubar>
 </template>
 
