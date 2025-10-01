5d74533fbe62603586fe819854ff532fa7a1d977
David Sawatzky <david@diatonic.io>
2025-09-30 22:03:48 -0500

    updated nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -52,8 +52,8 @@ export default defineNuxtConfig({
     },
     clients: {
       default: {
-        // httpEndpoint: 'https://wmfapi.diatonic.ca/graphql',
-        httpEndpoint: 'http://localhost:3000/graphql',
+        httpEndpoint:
+          process.env.NUXT_GRAPHQL_SERVER || 'http://localhost:3000/graphql',
         tokenName: 'diatonicToken',
         authHeader: 'Authorization',
         authType: 'Bearer',
