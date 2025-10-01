92109c1d3a64d52400b5448dba111d7e23068e90
David Sawatzky <david@diatonic.io>
2025-09-30 21:43:25 -0500

    updated apollo graphql endpoint

--- apollo.config.cjs
+++ apollo.config.cjs
@@ -3,8 +3,7 @@ module.exports = {
     service: {
       name: 'wmf-nest',
       // URL to the GraphQL API
-      // url: 'https://wmfapi.diatonic.ca/graphql',
-      url: 'http://localhost:3000/graphql',
+      url: process.env.NUXT_GRAPHQL_SERVER,
     },
     // Files processed by the extension
     includes: [
